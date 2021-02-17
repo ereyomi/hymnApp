05fb8595e4eba7ca0c95d26a85a1d8d5092aae48
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-02-17 02:08:59 +0100

    feat: ugraded app to version 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IndexedDbService } from './services/indexed-db.service';
-
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,18 +14,15 @@ import { IndexedDbService } from './services/indexed-db.service';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
     private indexedDb: IndexedDbService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.indexedDb.openIndexedDB();
+    
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.indexedDb.openIndexedDB();
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HymysApiService } from './services/hymys-api.service';
-import { IndexedDbService } from './services/indexed-db.service';
-import { FormsModule } from '@angular/forms';
-import { HymnspipeModule } from './pipe/hymnspipe.module';
-
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, HymnspipeModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    HymysApiService,
-    IndexedDbService,
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
